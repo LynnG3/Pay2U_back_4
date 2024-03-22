e24ac233c5ce792d95c690c475f61ed0f6f78fd6
Полина Грунина <linaborisovsky@yandex.ru>
2024-03-22 19:07:52 +0300

    добавлена админка services для категорий и сервисов; заготовка пуш уведомлений в прил payments

--- backend/services/admin.py
+++ backend/services/admin.py
@@ -6,40 +6,26 @@ from .models import Category, Service
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    """ Администрирование тегов. """
+    """ Администрирование категорий. """
 
-    list_display = ('name',)
-    search_fields = ('name',)
-    list_filter = ('name',)
-
-
-class CatInline(admin.TabularInline):
-    """Администрирование тегов к рецептам. """
-
-    model = Service.category.through
+    list_display = ('title',)
+    search_fields = ('title',)
+    list_filter = ('title',)
 
 
 @admin.register(Service)
 class ServiceAdmin(admin.ModelAdmin):
     """ Администрирование сервисов. """
 
-    inlines = (CatInline)
     list_display = (
-        # 'name',
-        # 'text',
-        # 'author',
-        # 'pub_date',
-        # 'cooking_time',
-        # 'display_ingredients',
-        # 'favorite_count',
-        # 'display_tags'
+        'name',
+        'category',
+        'text',
+        'cost',
+        'cashback',
+        'new',
+        'popular',
+        'pub_date'
     )
-    # search_fields = ('name', 'author__username', 'favorite_count')
-    # list_filter = ('name', 'author', 'tags')
-
-    # @admin.display(description='Теги')
-    # def display_tags(self, recipe):
-    #     return recipe.tags.values_list(
-    #         'name', flat=True
-    #     ).order_by('name')
-
+    search_fields = ('name', )
+    list_filter = ('name', )
